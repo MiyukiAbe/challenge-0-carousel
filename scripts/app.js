6ebf8ea48cdbc10b7714a3938b32bf728ba0a4dc
Miyuki Abe <miyuki0809@gmail.com>
2019-06-25 11:42:48 -0400

    feat: Both interal and buttons are working now

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,30 @@
-const image = document.getElementsByClassName('image');
 
 let curNum = 0
+const image = document.getElementsByClassName('image');
 
-//Below is button version 
-//const rotateDirection = document.getElementsByClassName('button')
-// const rotateImg = function () {
-//   let direction = event.target.value
-//   if (direction === 'left') {
-//     if (curNum === 0) curNum = 3;
-//     else curNum--
-//     }
-//   else if (direction === 'right') {
-//     if (curNum === 3) curNum = 0;
-//     else curNum++
-//   }
-//   image[0].style.backgroundImage = "url('./images/hero-image-" + curNum + ".jpg')"
-// }
+//Every 5 seconds image will change
 
-// rotateDirection[0].addEventListener('click', rotateImg)
-
-//Below is auto version
 const rotateImg = function () {
     if (curNum === 3) curNum = 0;
     else curNum++
+  image[0].style.backgroundImage = "url('./images/hero-image-" + curNum + ".jpg')"
+}
+setInterval(rotateImg, 5000)
 
+//User can change images by clicking buttons
+const changeImgByButton = function () {
+  let direction = event.target.value
+  if (direction === 'left') {
+    if (curNum === 0) curNum = 3;
+    else curNum--
+    }
+  else if (direction === 'right') {
+    if (curNum === 3) curNum = 0;
+    else curNum++
+  }
   image[0].style.backgroundImage = "url('./images/hero-image-" + curNum + ".jpg')"
 }
 
-setInterval(rotateImg, 1500)
+const rotateDirection = document.getElementsByClassName('button')
+rotateDirection[0].addEventListener('click', changeImgByButton)
+
