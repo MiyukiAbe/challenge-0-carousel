651f309314cb92c157fd1e2cbe5cfdfd5df8ba55
Miyuki Abe <miyuki0809@gmail.com>
2019-06-24 20:27:02 -0400

    feat: Implemented setInterval Version

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,30 @@
 const image = document.getElementsByClassName('image');
-const rotateDirection = document.getElementsByClassName('button')
 
 let curNum = 0
 
+//Below is button version 
+//const rotateDirection = document.getElementsByClassName('button')
+// const rotateImg = function () {
+//   let direction = event.target.value
+//   if (direction === 'left') {
+//     if (curNum === 0) curNum = 3;
+//     else curNum--
+//     }
+//   else if (direction === 'right') {
+//     if (curNum === 3) curNum = 0;
+//     else curNum++
+//   }
+//   image[0].style.backgroundImage = "url('./images/hero-image-" + curNum + ".jpg')"
+// }
+
+// rotateDirection[0].addEventListener('click', rotateImg)
+
+//Below is auto version
 const rotateImg = function () {
-  let direction = event.target.value
-  if (direction === 'left') {
-    if (curNum === 0) curNum = 3;
-    else curNum--
-    }
-  else if (direction === 'right') {
     if (curNum === 3) curNum = 0;
     else curNum++
-  }
+
   image[0].style.backgroundImage = "url('./images/hero-image-" + curNum + ".jpg')"
 }
 
-rotateDirection[0].addEventListener('click', rotateImg)
+setInterval(rotateImg, 1500)
